2e781890826923b73748c9322f9e6c396944d204
Nayan Jyoti Nath <nayannath2142@gmail.com>
2025-04-10 23:36:10 +0530

    1st

---
+++ learning-components/src/App.jsx
@@ -0,0 +1,13 @@
+import Usercard from "./components/Usercard"
+
+function App() {
+  return (
+    <div>
+      <Usercard />
+      <Usercard />
+      <Usercard />
+    </div>
+  )
+}
+
+export default App
---
+++ learning-components/src/components/Usercard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Usercard = () => {
+  return (
+    <div className='container'>
+      <p id='name'>Nayan</p>
+      <img id='userimg' src="" alt="love" />
+      <p id='desc'>Description of Components</p>
+    </div>
+  )
+}
+
+export default Usercard
